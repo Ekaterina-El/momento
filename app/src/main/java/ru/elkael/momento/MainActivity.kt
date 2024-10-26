2974b30a216826641bc470c56ec4a123b3aec3f5
Elshina Ekaterina <Ekaterina.Elshina@pprcard.ru>
2024-10-26 17:18:31 +0500

    Init

---
+++ app/src/main/java/ru/elkael/momento/MainActivity.kt
@@ -0,0 +1,24 @@
+package ru.elkael.momento
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import ru.elkael.momento.ui.theme.MomentoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MomentoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
